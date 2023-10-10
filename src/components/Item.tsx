3e2b01e9d93161daf8c50fd632816bedaf5580f7
ivandmitrienko <oneblackcrystal@gmail.com>
2023-10-10 21:59:20 +0300

    initial commit

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -6,19 +6,31 @@ import { CardContent, CardMedia, Typography } from '@mui/material';
 export const Item = (el: IData) => {
     return (
         <div>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{
+                width: 260,
+                maxHeight: 320,
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
                 <CardMedia
-                    sx={{ height: 140}}
+                    component="img"
+                    sx={{ height: 200, objectFit: "contain" }}
                     image={el.image}
                     title={el.category}
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography gutterBottom variant="subtitle2" component="div">
                         {el.title}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                       {el.description}
+                    <Typography
+                        variant="body2"
+                        color="text.secondary"
+                    >
+                        ${el.price}
                     </Typography>
+                    <small><del>{(Number(el.price) * 1.25).toFixed(2)}</del></small>
                 </CardContent>
             </Card>
         </div>
